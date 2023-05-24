970682b887df75675c086497718456058078d836
Sagar Chawla <sagarchawla83@gmail.com>
2023-05-24 20:18:10 +0530

    added home page for backend

--- index.js
+++ index.js
@@ -2,14 +2,19 @@ import express from 'express'
 import cors from 'cors'
 import http from 'http'
 import { Server } from 'socket.io'
-import path from 'path'
+import { fileURLToPath } from 'url';
+import path from 'path';
 
 const app = express();
+app.use(cors())
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const server = http.createServer(app);
 const io = new Server(server, {
     cors:{
-        origin : 'https://quickchat.netlify.app'
+        origin : '*'
     }
 })
 
